8b4eb67319605f5823b4289be258f0651cdad74f
hakyeonghoon1 <skyhoon8@gmail.com>
2021-12-02 11:57:05 +0900

    Emaillist

--- emaillist/src/App.js
+++ emaillist/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './assets/scss/App.scss';
+import Emaillist from './Emaillist';
 import RegisterForm from './RegisterForm';
+import SearchBar from './SearchBar';
 
 export default function(){
     return(
         <div className='App'>
             <RegisterForm/>
+            <SearchBar/>
+            <Emaillist/>
         </div>
     )
 }
