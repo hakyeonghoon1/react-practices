e4cf43a2b5652bc7530aae1b962a943806d797e9
hakyeonghoon1 <skyhoon8@gmail.com>
2021-12-02 11:17:59 +0900

    emaillist

--- guestbook/config/webpack.config.js
+++ guestbook/config/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = function(env){
                 test:/\.(sa|sc|c)ss$/i,
                 use:[
                     'style-loader',
-                    {loader:'css-loader',options:{modules:'true'}},
+                    {loader:'css-loader',options:{modules:true}},
                     'sass-loader'
                 ] //순서 중요
             },{
