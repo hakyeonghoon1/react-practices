3a715d64cbff59092c894a200a970ce6bcd244da
hakyeonghoon1 <skyhoon8@gmail.com>
2021-12-07 11:24:48 +0900

    fetch

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,23 +1,57 @@
-import React, { useState } from 'react';
-import './assets/scss/App.scss';
-import Emaillist from './Emaillist';
+import React, {useState, useEffect} from 'react';
 import RegisterForm from './RegisterForm';
 import SearchBar from './SearchBar';
+import Emaillist from './Emaillist';
 
-import data from './assets/json/data.json';
+import './assets/scss/App.scss';
+// import data from './assets/json/data.json';
 
-export default function(){
-    const [keyword,setKeyword] = useState('');
-    const notifyKeywordChanged = (keyword) =>{
-        console.log(keyword);
-        setKeyword(keyword);
+export default function() {
+    const [emails, setEmails] = useState([]);
+    const [keyword, setKeyword] = useState('');
+
+    const notifyKeywordChanged = (keyword) => {
+      setKeyword(keyword);
     };
 
-    return(
-        <div className='App'>
-            <RegisterForm/>
-            <SearchBar keyword={keyword} callback={notifyKeywordChanged}/>
-            <Emaillist keyword={keyword} emails={data}/>
+    useEffect(async () => {
+      try {
+        const response = await fetch('/api', {
+          method: 'get',
+          mode:'cors',                          // no-cors, cors, same-origin
+          credentials:'include',                // include, omit, same-origin
+          cache:'no-cache',                     // no-cache, reload, force-cache, default*
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application'
+          },
+          redirect:'follow',                    // follow*, error, manual(response.url)
+          referrer:'client',                    // no-refferer, *client
+          body: null
+        });
+
+        if(!response.ok) {
+          throw new Error(`${response.status} ${response.statusText}`);
+        }
+
+        const jsonResult = await response.json();
+        console.log(jsonResult);
+        if(jsonResult.result !== 'success') {
+          throw new Error(`${jsonResult.result} ${jsonResult.message}`);
+        }
+
+        setEmails(jsonResult.data);
+
+      } catch (err) {
+        console.error(err);
+      }
+    }, []);
+
+    return (
+        <div className={'App'}>
+          <RegisterForm />
+          <SearchBar keyword={keyword} callback={notifyKeywordChanged} />
+          <Emaillist keyword={keyword} emails={emails} />  
         </div>
     )
 }
